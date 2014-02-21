709211ff8c8fa16d943702b28773a3cd642b687c
Kevin Miller <Airswoop1@gmail.com>
2014-02-21 16:24:36 -0500

    pulled qa into master and changed endpoints

--- src/com/chatwala/android/EnvironmentVariables.java
+++ src/com/chatwala/android/EnvironmentVariables.java
@@ -39,7 +39,7 @@ public enum EnvironmentVariables {
 
     public static EnvironmentVariables get()
     {
-        return EnvironmentVariables.QA;
+        return EnvironmentVariables.PROD;
     }
 
 
