bbac173fdf7f9bc2014e9eec73cef2012f71aa14
Eliezer Graber <eygraber@gmail.com>
2014-03-02 17:46:44 -0500

    Load the contacts and display them in a list.

--- src/com/chatwala/android/activity/SmsActivity.java
+++ src/com/chatwala/android/activity/SmsActivity.java
@@ -1,25 +1,43 @@
 package com.chatwala.android.activity;
 
-import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.database.Cursor;
+import android.database.DatabaseUtils;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.support.v4.widget.CursorAdapter;
+import android.support.v4.widget.SimpleCursorAdapter;
 import android.telephony.SmsManager;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.EditText;
+import android.widget.FilterQueryProvider;
+import android.widget.ListView;
 import com.chatwala.android.R;
 
-public class SmsActivity extends Activity {
+public class SmsActivity extends FragmentActivity implements LoaderManager.LoaderCallbacks<Cursor> {
     public static final String SMS_MESSAGE_EXTRA = "sms_message";
 
-    private static final int CONTACTS_REQUEST_CODE = 0;
+    private static final int CONTACTS_LOADER_CODE = 0;
 
-    private EditText smsMessageBox;
-    private EditText contactsBox;
+    private EditText contactsFilter;
+    private ListView contactsList;
+
+    private CursorAdapter adapter;
+
+    private String[] contactsProjection = new String[] {ContactsContract.CommonDataKinds.Phone._ID,
+                                                        ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
+                                                        ContactsContract.CommonDataKinds.Phone.NUMBER};
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -31,17 +49,51 @@ public class SmsActivity extends Activity {
             return;
         }
 
-        String message = getIntent().getStringExtra(SMS_MESSAGE_EXTRA);
-        smsMessageBox = (EditText) findViewById(R.id.message_text);
-        smsMessageBox.setText(message);
+        //String message = getIntent().getStringExtra(SMS_MESSAGE_EXTRA);
 
-        contactsBox = (EditText) findViewById(R.id.add_contacts_box);
-
-        findViewById(R.id.add_contact_button).setOnClickListener(new View.OnClickListener() {
+        contactsFilter = (EditText) findViewById(R.id.contacts_filter);
+        contactsFilter.addTextChangedListener(new TextWatcher() {
             @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.CommonDataKinds.Phone.CONTENT_URI);
-                startActivityForResult(intent, CONTACTS_REQUEST_CODE);
+            public void afterTextChanged(final Editable s) {
+                contactsFilter.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        adapter.getFilter().filter(s.toString());
+                    }
+                }, 300);
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i2, int i3) {}
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i2, int i3) {}
+        });
+
+        contactsList = (ListView) findViewById(R.id.contacts_list);
+        adapter = new SimpleCursorAdapter(this,
+                                          R.layout.layout_contact,
+                                          null,
+                                          new String[] { ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME, ContactsContract.CommonDataKinds.Phone.NUMBER },
+                                          new int[] { R.id.contact_item_name, R.id.contact_item_number },
+                                          0);
+        adapter.setFilterQueryProvider(new FilterQueryProvider() {
+            @Override
+            public Cursor runQuery(CharSequence filter) {
+                filter = DatabaseUtils.sqlEscapeString(filter.toString() + "%");
+                return getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                                                  contactsProjection,
+                                                  ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME + " LIKE " + filter + " OR " +
+                                                  ContactsContract.CommonDataKinds.Phone.NUMBER + " LIKE " + filter,
+                                                  null,
+                                                  ContactsContract.CommonDataKinds.Phone.SORT_KEY_PRIMARY);
+            }
+        });
+        contactsList.setAdapter(adapter);
+
+        contactsList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
             }
         });
 
@@ -51,67 +103,38 @@ public class SmsActivity extends Activity {
                 sendSms();
             }
         });
-    }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if(resultCode == RESULT_OK) {
-            if(requestCode == CONTACTS_REQUEST_CODE) {
-                Uri uri = data.getData();
-
-                if(uri != null) {
-                    Cursor c = null;
-                    String nameCol = ContactsContract.Contacts.DISPLAY_NAME;
-                    String numberCol = ContactsContract.CommonDataKinds.Phone.NUMBER;
-                    try {
-                        c = getContentResolver().query(uri, new String[] {nameCol, numberCol},
-                                null, null, null);
-                        if(c != null && c.moveToFirst()) {
-                            int nameIndex = c.getColumnIndex(nameCol);
-                            int numberIndex = c.getColumnIndex(numberCol);
-                            if(numberIndex != -1) {
-                                String name =  null;
-                                try {
-                                    name = c.getString(nameIndex);
-                                }
-                                catch(Exception ignore) {}
-                                String number = c.getString(numberIndex);
-
-                                if(!contactsBox.getText().toString().trim().isEmpty()) {
-                                    contactsBox.append("; ");
-                                }
-
-                                if(name != null) {
-                                    contactsBox.append("<" + name + ">");
-                                }
-
-                                contactsBox.append(number);
-                            }
-                        }
-                    }
-                    finally {
-                        if(c != null && !c.isClosed()) {
-                            c.close();
-                        }
-                    }
-                }
-            }
-        }
+        getSupportLoaderManager().initLoader(CONTACTS_LOADER_CODE, null, this);
     }
 
     private void sendSms() {
-        String[] addresses = contactsBox.getText().toString().split(";");
-        new SendSmsAsyncTask().execute(addresses);
+        //String[] addresses = contactsBox.getText().toString().split(";");
+        //new SendSmsAsyncTask().execute(addresses);
     }
 
-    private int getIndexOfLastBracket(String s) {
-        int bracketIndex = 0;
-        for(int i = 0; i < s.length(); i++) {
-            if(s.charAt(i) == '>') {
-                bracketIndex = i;
-            }
+    @Override
+    public Loader<Cursor> onCreateLoader(int loaderCode, Bundle bundle) {
+        if(loaderCode == CONTACTS_LOADER_CODE) {
+            return new CursorLoader(SmsActivity.this,
+                                    ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                                    contactsProjection,
+                                    null,
+                                    null,
+                                    ContactsContract.CommonDataKinds.Phone.SORT_KEY_PRIMARY);
         }
-        return bracketIndex;
+        else {
+            return null;
+        }
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> cursorLoader, Cursor cursor) {
+        adapter.changeCursor(cursor);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> cursorLoader) {
+        adapter.changeCursor(null);
     }
 
     private class SendSmsAsyncTask extends AsyncTask<String, Void, Void> {
@@ -125,13 +148,13 @@ public class SmsActivity extends Activity {
 
         @Override
         protected Void doInBackground(String... addresses) {
-            for(String address : addresses) {
+            /*for(String address : addresses) {
                 int lastBracketIndex = getIndexOfLastBracket(address);
                 if(lastBracketIndex > 0) {
                     address = address.substring(getIndexOfLastBracket(address) + 1).trim();
                 }
                 SmsManager.getDefault().sendTextMessage(address, null, smsMessageBox.getText().toString(), null, null);
-            }
+            }*/
 
             return null;
         }
