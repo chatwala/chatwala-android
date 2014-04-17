15da1b249aab7b912871a68ec3606ce5977298f8
Eliezer Graber <eli@chatwala.com>
2014-04-17 16:40:08 -0400

    Fixed the issue with low res profile pics.

--- src/com/chatwala/android/activity/UpdateProfilePicActivity.java
+++ src/com/chatwala/android/activity/UpdateProfilePicActivity.java
@@ -1,9 +1,14 @@
 package com.chatwala.android.activity;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.MenuItem;
 import android.view.View;
@@ -15,19 +20,18 @@ import com.chatwala.android.AppPrefs;
 import com.chatwala.android.R;
 import com.chatwala.android.dataops.DataProcessor;
 import com.chatwala.android.superbus.server20.UploadUserProfilePictureCommand;
+import com.chatwala.android.util.Logger;
 import com.chatwala.android.util.MessageDataStore;
+import com.chatwala.android.util.ThumbUtils;
 import com.squareup.picasso.Picasso;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.IOException;
 
 /**
  * Created by matthewdavis on 1/10/14.
  */
-public class UpdateProfilePicActivity extends BaseChatWalaActivity
-{
+public class UpdateProfilePicActivity extends BaseChatWalaActivity {
     private String userId;
     private boolean isReview;
     private ImageView profilePicImage;
@@ -39,8 +43,7 @@ public class UpdateProfilePicActivity extends BaseChatWalaActivity
     private static final String IS_REVIEW = "IS_REVIEW";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_profile_pic);
 
@@ -52,12 +55,10 @@ public class UpdateProfilePicActivity extends BaseChatWalaActivity
         profilePicImage = (ImageView)findViewById(R.id.current_profile_pic);
         noImageText = (TextView)findViewById(R.id.no_profile_pic_text);
         File thumbImage = MessageDataStore.findUserImageInLocalStore(userId);
-        if(thumbImage.exists())
-        {
+        if(thumbImage.exists()) {
             Picasso.with(UpdateProfilePicActivity.this).load(thumbImage).skipMemoryCache().resize(350, 250).centerCrop().noFade().into(profilePicImage);
         }
-        else
-        {
+        else {
             profilePicImage.setVisibility(View.GONE);
             noImageText.setVisibility(View.VISIBLE);
         }
@@ -69,66 +70,73 @@ public class UpdateProfilePicActivity extends BaseChatWalaActivity
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                //tempThumbImage = MessageDataStore.makeTempUserFile(userId);
-                //intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(tempThumbImage));
+                tempThumbImage = new File(Environment.getExternalStorageDirectory(), "chatwala-" + userId + ".png");
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(tempThumbImage));
                 startActivityForResult(intent, TAKE_PICTURE_REQUEST);
             }
         });
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data)
-    {
-        if(requestCode == TAKE_PICTURE_REQUEST && resultCode == RESULT_OK)
-        {
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == TAKE_PICTURE_REQUEST && resultCode == RESULT_OK) {
             profilePicImage.setVisibility(View.VISIBLE);
             noImageText.setVisibility(View.GONE);
 
             newThumbImage = MessageDataStore.findUserImageInLocalStore(userId);
-            if(newThumbImage.exists())
-            {
-                newThumbImage.delete();
-            }
 
-            try
-            {
-                FileOutputStream out = new FileOutputStream(newThumbImage);
-                ((Bitmap)data.getExtras().get("data")).compress(Bitmap.CompressFormat.PNG, 100, out);
-                out.close();
+            if(tempThumbImage.exists()) {
+                final ProgressDialog pd = ProgressDialog.show(this, "Saving Image", "Please Wait...", true);
+                new AsyncTask<Void, Void, Boolean>() {
 
-                DataProcessor.runProcess(new Runnable() {
                     @Override
-                    public void run() {
-                        BusHelper.submitCommandSync(UpdateProfilePicActivity.this, new UploadUserProfilePictureCommand(newThumbImage.getPath()));
+                    protected Boolean doInBackground(Void... voids) {
+                        try {
+                            Bitmap thumbBitmap = BitmapFactory.decodeFile(tempThumbImage.getPath());
+                            thumbBitmap = ThumbUtils.rotateBitmap(tempThumbImage, thumbBitmap);
+                            thumbBitmap.compress(Bitmap.CompressFormat.PNG, 100, new FileOutputStream(newThumbImage));
+
+                            tempThumbImage.delete();
+
+                            DataProcessor.runProcess(new Runnable() {
+                                @Override
+                                public void run() {
+                                    BusHelper.submitCommandSync(UpdateProfilePicActivity.this, new UploadUserProfilePictureCommand(newThumbImage.getPath()));
+                                }
+                            });
+                            return true;
+                        }
+                        catch(Exception e) {
+                            Logger.e("Couldn't save new profile picture", e);
+                            return false;
+                        }
                     }
-                });
 
-                Toast.makeText(UpdateProfilePicActivity.this, "Profile updated!", Toast.LENGTH_LONG).show();
+                    @Override
+                    public void onPostExecute(Boolean result) {
+                        if(result) {
+                            Toast.makeText(UpdateProfilePicActivity.this, "Profile updated!", Toast.LENGTH_LONG).show();
+                        }
+                        else {
+                            Toast.makeText(UpdateProfilePicActivity.this, "Problem updating profile, please try again", Toast.LENGTH_LONG).show();
+                        }
+                        if(pd != null && pd.isShowing()) {
+                            pd.dismiss();
+                        }
+                        Picasso.with(UpdateProfilePicActivity.this).load(newThumbImage).skipMemoryCache().resize(350, 250).centerCrop().noFade().into(profilePicImage);
+                    }
+                }.execute();
             }
-            catch (FileNotFoundException e)
-            {
-                Toast.makeText(UpdateProfilePicActivity.this, "Problem updating profile, please try again", Toast.LENGTH_LONG).show();
-            }
-            catch (IOException e)
-            {
-                Toast.makeText(UpdateProfilePicActivity.this, "Problem updating profile, please try again", Toast.LENGTH_LONG).show();
-            }
-
-            UpdateProfilePicActivity.startMe(UpdateProfilePicActivity.this, isReview);
-            finish();
 
         }
-        else
-        {
+        else {
             super.onActivityResult(requestCode, resultCode, data);
         }
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item)
-    {
-        switch (item.getItemId())
-        {
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
             case android.R.id.home:
                 onBackPressed();
                 return true;
@@ -138,22 +146,18 @@ public class UpdateProfilePicActivity extends BaseChatWalaActivity
     }
 
     @Override
-    public void onBackPressed()
-    {
-        if(isReview)
-        {
+    public void onBackPressed() {
+        if(isReview) {
             AppPrefs.getInstance(UpdateProfilePicActivity.this).setImageReviewed();
             NewCameraActivity.startMe(UpdateProfilePicActivity.this);
             finish();
         }
-        else
-        {
+        else {
             super.onBackPressed();
         }
     }
 
-    public static void startMe(Context context, boolean isReview)
-    {
+    public static void startMe(Context context, boolean isReview) {
         Intent intent = new Intent(context, UpdateProfilePicActivity.class);
         intent.putExtra(IS_REVIEW, isReview);
         context.startActivity(intent);
